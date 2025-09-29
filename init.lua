946b4dd501e4671c6dbb7dc94136326e8119e1d5
Edoardo Oranger <edo.oranger@gmail.com>
2025-09-29 21:19:04 +0200

    feat: Lazy Setup
    Completed setup of Lazy package manager

--- init.lua
+++ init.lua
@@ -19,3 +19,21 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
+
+-- Setup of Lazy.nvim
+
+local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(path) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        path,
+    })
+end
+
+vim.opt.rtp:prepend(path)
+
