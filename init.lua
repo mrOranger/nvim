b07d055bdae5ea5e04ebf3bfa770779433a0000c
Edoardo Oranger <edo.oranger@gmail.com>
2025-09-29 21:11:06 +0200

    feat: Initial Options
    Setup of initial configuration for NeoVim options.

---
+++ init.lua
@@ -0,0 +1,21 @@
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.splitbelow = false
+vim.opt.splitright = true
+
+vim.opt.wrap = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
