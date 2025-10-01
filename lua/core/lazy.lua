e825d3829ef025decdac6bdaca16a3ffeace606f
Edoardo Oranger <edo.oranger@gmail.com>
2025-10-01 13:19:24 +0200

    feat: Telescope
    Add Telescope plugin with keybindings

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
     { 
         "rebelot/kanagawa.nvim",
         config = function ()
-            vim.cmd.colorscheme("kanagawa-wave")
+            vim.cmd.colorscheme("kanagawa-dragon")
         end,
     },
     {
@@ -60,5 +60,17 @@ require("lazy").setup({
                 },
             })
         end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", },
+        config = function ()
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+        end,
+    },
+    {
     },
 })
