922c4fd41c21cd45e4788e38fa3be8064a04742e
Edoardo Oranger <edo.oranger@gmail.com>
2025-10-01 16:40:21 +0200

    fix: Column Option
    Fix error in column option

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,6 +8,7 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.colorcolumn = "120"
 
 vim.opt.clipboard = "unnamedplus"
 
@@ -18,7 +19,7 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
-vim.opt.scroloff = 8
+vim.opt.scrolloff = 8
 
 vim.opt.wrap = true
 vim.opt.smartindent = true
