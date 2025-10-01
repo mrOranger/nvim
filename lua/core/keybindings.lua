8fd6a8cd13ce1ff0531dac8950f0d0c305187873
Edoardo Oranger <edo.oranger@gmail.com>
2025-10-01 16:40:37 +0200

    feat: New Keybindings
    Add new keybindings for scrolling and switch buffer

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -7,3 +7,10 @@ vim.keymap.set("n", "<leader>w!", ":w!<CR>", { desc = "Force Write" })
 vim.keymap.set("n", "<leader>x", ":qa<CR>", { desc = "Quit All" })
 vim.keymap.set("n", "<leader>X", ":qa!<CR>", { desc = "Quite All Without Save" })
 
+vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
+
+vim.api.nvim_set_keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+
+
