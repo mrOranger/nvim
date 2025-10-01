2411edcc93ea3fd322c835bae1b39e7cc0e861a6
Edoardo Oranger <edo.oranger@gmail.com>
2025-10-01 16:35:38 +0200

    feat: New Options
    Add new Nvim options for tab and indentation

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,10 +4,9 @@ vim.opt.relativenumber = false
 vim.opt.splitbelow = false
 vim.opt.splitright = true
 
-vim.opt.wrap = true
-
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.opt.clipboard = "unnamedplus"
@@ -19,3 +18,9 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
+vim.opt.scroloff = 8
+
+vim.opt.wrap = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
