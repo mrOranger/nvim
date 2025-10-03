0e950cd11a387639316ec36f2e01663b69f1126c
Edoardo Oranger <edo.oranger@gmail.com>
2025-10-03 13:35:15 +0200

    feat: Completion Configuration
    Add Configuration of Completion Engine

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,17 +1,20 @@
-local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(path) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        path,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazy_path) then
+    local lazy_repo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazy_repo, lazy_path })
+    if (vim.v.shell_error ~= 0) then 
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim: \n"},
+            { out, "WarningMsg" },
+            { "\nPress any key to exit ..."},
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
-vim.opt.rtp:prepend(path)
+vim.opt.rtp:prepend(lazy_path)
 
 require("lazy").setup({
     {
@@ -71,7 +74,7 @@ require("lazy").setup({
         "catppuccin/nvim",
         disabled = true,
         config = function ()
-            vim.cmd.colorscheme("catppuccin-mocha")
+            -- vim.cmd.colorscheme("catppuccin-mocha")
         end,
     },
     {
@@ -188,5 +191,18 @@ require("lazy").setup({
                 },
             })
         end,
+    },
+    {
+       "saghen/blink.cmp",
+       dependencies = { "rafamadriz/friendly-snippets" },
+       version = "1.x",
+       opts = {
+           keyamp = { present = "super-tab" },
+           appearance = { nerd_font_variant = "mono" },
+           completion = { documentation = { auto_show = true } },
+           sources = { default = { "lsp", "path", "snippets", "buffer" }},
+           fuzzy = { implementation = "prefer_rust_with_warning" },
+       },
+       opts_extend = { "sources.default" },
     },
 })
