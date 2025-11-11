8c84b19ded1125342bd59b23f30eefcf71e7acac
Edoardo Oranger <edo.oranger@gmail.com>
2025-11-11 20:58:26 +0100

    feat: Updated Lualine
    Add file relative path

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -8,7 +8,15 @@ return {
                 sections = {
                     lualine_a = { { "mode", icon = '', } },
                     lualine_b = { "diff" },
-                    lualine_c = { "filename", "filetype", "encoding", "filesize" },
+                    lualine_c = { 
+                        {
+                            "filename",
+                            path = 1,
+                        },
+                        "filetype",
+                        "encoding",
+                        "filesize"
+                    },
                     lualine_x = { },
                     lualine_y = { "location", "progress" },
                     lualine_z = { "lsp_status" },
