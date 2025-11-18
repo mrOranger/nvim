821ce8e340e81b4937cdf708ae976fa4018da371
Edoardo Oranger <e.oranger@reply.it>
2025-11-18 09:42:31 +0100

    feat: Updated Config
    Enabled LSP for C with clangd

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -8,10 +8,28 @@ return {
             "williamboman/mason.nvim",
         },
         config = function ()
-            require("mason").setup({})
+            require("mason").setup({
+                automatic_enable = true,
+            })
+
+            require("mason-lspconfig").setup({
+                ensure_installed = { "clangd" }
+            })
+
             vim.diagnostic.config({
                 virtual_text = true,
                 virtual_line = true,
+            })
+            
+            vim.lsp.start({
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    "--header-insertion=iwyu",
+                    "--completion-style=detailed",
+                    "--function-arg-placeholders",
+                }
             })
         end,
     }
